db1697bedaa6509427a0d3cf38aef976ebcea08d
Patrick Shobe <patrick.shobe@housecallpro.com>
2022-08-11 08:01:27 -0600

    erb views

--- app/quotes_controller.rb
+++ app/quotes_controller.rb
@@ -2,4 +2,19 @@ class QuotesController < R00lz::Controller
   def a_quote
     "What's up, Doc?"
   end
+
+  def shakes
+    @noun = :winking
+    render(:shakes)
+  end
+
+  def card_trick
+    n = params['card'] || 'Queen'
+    "Your card: the #{n} of spades!"
+  end
+
+  def fq
+    @q = FileModel.find(1)
+    render :quote
+  end
 end
